cc57974edb6b8e5998ebc6b24a6c1234153534b6
Albert Marashi <albert@insanemarketing.com.au>
2021-04-05 10:02:28 +0930

    Move to new selection system

--- src/view/ViewDescription.ts
+++ src/view/ViewDescription.ts
@@ -1,17 +1,41 @@
 import type { Node as EditorNode } from '../editor/node'
-import type { View } from '../editor/node'
+import type { View } from '../view/View'
+
+type HTMLNode = Node & { viewDesc: ViewDesc }
 
 export class ViewDesc {
-    el: Node
+    #el: HTMLNode
     parent: ViewDesc
     children: ViewDesc[]
 
-    constructor(parent: ViewDesc | null, children: ViewDesc[], el: Node){
+    constructor(parent: ViewDesc | null, children: ViewDesc[], el: HTMLNode){
         this.parent = parent
         this.children = children
         this.el = el
+    }
 
-        el.ViewDesc = this
+    get el () {
+		return this.#el
+	}
+	
+	set el (value) {
+		if(value) value.viewDesc = this
+		this.#el = value
+        if(value){
+            this.updateChildren()
+        }
+	}
+
+    get size() {
+        let size = 0
+        for(let i = 0; i < this.children.length; i++){
+            size += this.children[i].size
+        }
+        return size
+    }
+
+    updateChildren(){
+		this.children = (Array.from(this.el.childNodes) as unknown as HTMLNode[]).filter(node => node.viewDesc).map(n => n.viewDesc)
     }
 }
 
@@ -19,10 +43,20 @@ export class NodeViewDesc extends ViewDesc {
     node: EditorNode
     view: View
 
-    constructor(parent: ViewDesc | null, children: ViewDesc[], el: Node, node: EditorNode, view: View){
+    constructor(parent: ViewDesc | null, children: ViewDesc[], el: HTMLNode, node: EditorNode, view: View){
         super(parent, children, el)
 
         this.node = node
         this.view = view
+    }
+
+    get size () {
+        return this.node.size
+    }
+}
+
+export class TextViewDesc extends NodeViewDesc {
+    constructor(parent: ViewDesc, el: HTMLNode, node: EditorNode, view: View){
+        super(parent, null, el, node, view)
     }
 }
